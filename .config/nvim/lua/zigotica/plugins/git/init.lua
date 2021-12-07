1537ef30c230d6778a2598ad17c86bf697273441
zigotica <zigotica@gmail.com>
2021-12-08 00:34:23 +0100

    GitSigns custom colors

--- .config/nvim/lua/zigotica/plugins/git/init.lua
+++ .config/nvim/lua/zigotica/plugins/git/init.lua
@@ -1,3 +1,7 @@
+vim.cmd[[ highlight GitSignsAdd    guifg=#97b87b ]]
+vim.cmd[[ highlight GitSignsChange guifg=#7496b8 ]]
+vim.cmd[[ highlight GitSignsDelete guifg=#c98b65 ]]
+
 require('gitsigns').setup{
   signs = {
     add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
