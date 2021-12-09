7cd1b78b4370de445c742d0d64e68d869dcbe9e9
zigotica <zigotica@gmail.com>
2021-12-09 19:58:04 +0100

    telescope to use file_ignore_patterns, tweak ff mapping

--- .config/nvim/lua/zigotica/plugins/telescope/init.lua
+++ .config/nvim/lua/zigotica/plugins/telescope/init.lua
@@ -1,29 +1,39 @@
 require('telescope').setup{
-defaults = {
-  prompt_prefix = "z: ",
-  mappings = {-- change default Enter to open in right split
-  n = {
-    ["<CR>"] = "select_vertical"
+  defaults = {
+    prompt_prefix = "z: ",
+    mappings = {-- change default Enter to open in right split
+      n = {
+        ["<CR>"] = "select_vertical"
+      },
+      i = {
+        ["<CR>"] = "select_vertical",
+        ["<C-h>"] = "which_key"
+      }
     },
-  i = {
-    ["<CR>"] = "select_vertical",
-    ["<C-h>"] = "which_key"
-    }
-  }
-},
+    file_ignore_patterns = {
+      "node_modules",
+      "dist",
+      "vendor",
+      "build",
+      "history",
+      "plugged",
+      ".DS_Store"
+    },
+  },
   pickers = {
     find_files = {
       find_command=rg,
-      },
+    },
     buffers = {
       mappings = {-- restore default Enter to open in same split
-      n = {
-        ["<CR>"] = "select_default"
+        n = {
+          ["<CR>"] = "select_default"
         },
-      i = {
-        ["<CR>"] = "select_default"
+        i = {
+          ["<CR>"] = "select_default"
         }
       }
     }
-  }
+  },
 }
+
