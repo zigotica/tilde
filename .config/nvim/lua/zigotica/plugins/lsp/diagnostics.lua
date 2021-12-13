984dfb9428c9576bf75902cfc78862457b4fe37d
zigotica <zigotica@gmail.com>
2021-12-13 19:40:29 +0100

    diagnostics foating win border using severity level color

--- .config/nvim/lua/zigotica/plugins/lsp/diagnostics.lua
+++ .config/nvim/lua/zigotica/plugins/lsp/diagnostics.lua
@@ -12,9 +12,46 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
+-- wrap open_float to inspect diagnostics and use the severity color for border
+-- https://neovim.discourse.group/t/lsp-diagnostics-how-and-where-to-retrieve-severity-level-to-customise-border-color/1679
+vim.diagnostic.open_float = (function(orig)
+    return function(bufnr, opts)
+        local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+        local opts = opts or {}
+        -- A more robust solution would check the "scope" value in `opts` to
+        -- determine where to get diagnostics from, but if you're only using
+        -- this for your own purposes you can make it as simple as you like
+        local diagnostics = vim.diagnostic.get(opts.bufnr or 0, {lnum = lnum})
+        local max_severity = vim.diagnostic.severity.HINT
+        for _, d in ipairs(diagnostics) do
+            -- Equality is "less than" based on how the severities are encoded
+            if d.severity < max_severity then
+                max_severity = d.severity
+            end
+        end
+        local border_color = ({
+            [vim.diagnostic.severity.HINT]  = "DiagnosticHint",
+            [vim.diagnostic.severity.INFO]  = "DiagnosticInfo",
+            [vim.diagnostic.severity.WARN]  = "DiagnosticWarn",
+            [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+        })[max_severity]
+        opts.border = {
+            {"🭽", border_color},
+            {"▔", border_color},
+            {"🭾", border_color},
+            {"▕", border_color},
+            {"🭿", border_color},
+            {"▁", border_color},
+            {"🭼", border_color},
+            {"▏", border_color},
+        }
+        orig(bufnr, opts)
+    end
+end)(vim.diagnostic.open_float)
+
 -- Show line diagnostics in floating popup on hover, except insert mode (CursorHoldI)
 vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]]
+vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float()]]
 
 -- Show source in diagnostics, not inline but as a floating popup
 vim.diagnostic.config({
@@ -23,25 +60,4 @@ vim.diagnostic.config({
     source = "always",  -- Or "if_many"
   },
 })
-
--- create an array to hold custom border styles
-local border = {
-      {"🭽", "LspFloatWinBorder"},
-      {"▔", "LspFloatWinBorder"},
-      {"🭾", "LspFloatWinBorder"},
-      {"▕", "LspFloatWinBorder"},
-      {"🭿", "LspFloatWinBorder"},
-      {"▁", "LspFloatWinBorder"},
-      {"🭼", "LspFloatWinBorder"},
-      {"▏", "LspFloatWinBorder"},
-}
-
--- modify open_floating_preview to use the custom borders
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-local open_floating_preview_custom = function(contents, syntax, opts, ...)
-  opts = opts or {}
-  opts.border = opts.border or border
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
-vim.lsp.util.open_floating_preview = open_floating_preview_custom
 
