cc3dd63aadec7b9f6b513930cfdb5f93dd6982c8
zigotica <zigotica@gmail.com>
2023-02-05 12:41:01 +0100

    open_on_setup deprecated, replace with custom autocmd

--- .config/nvim/lua/zigotica/plugins/menu/init.lua
+++ .config/nvim/lua/zigotica/plugins/menu/init.lua
@@ -1,5 +1,4 @@
 require'nvim-tree'.setup {
-  open_on_setup       = true,
   update_cwd          = true,
   diagnostics = {
     enable = true,
@@ -43,3 +42,24 @@ require'nvim-tree'.setup {
     }
   }
 }
+
+local function open_nvim_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
