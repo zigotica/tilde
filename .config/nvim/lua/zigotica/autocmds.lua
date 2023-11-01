cc8370108e5866f00a14b4cc843f8836e01375ae
zigotica <zigotica@gmail.com>
2023-11-01 15:33:25 +0100

    Telescope folds and keymap changes

--- .config/nvim/lua/zigotica/autocmds.lua
+++ .config/nvim/lua/zigotica/autocmds.lua
@@ -50,4 +50,7 @@ augroup numbertoggle
   autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
   autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
 augroup END
+
+" disable folds inside Telesope picker
+autocmd! FileType TelescopeResults setlocal nofoldenable
 ]])
