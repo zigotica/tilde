81567b80f359122c138e0fba9dac4f8a295b346e
zigotica <zigotica@gmail.com>
2023-11-01 21:28:18 +0100

    simplify autocmds and convert remaining to lua

--- .config/nvim/lua/zigotica/autocmds.lua
+++ .config/nvim/lua/zigotica/autocmds.lua
@@ -1,56 +1,10 @@
-vim.cmd([[
-" ripped off from https://github.com/wincent/wincent just simplified & renames
-let g:WincentColorColumnBufferNameBlacklist = ['__LanguageClient__']
-let g:WincentColorColumnFileTypeBlacklist = ['command-t', 'diff', 'dirvish', 'fugitiveblame', 'nerdtree', 'undotree', 'qf']
-let g:WincentCursorlineBlacklist = ['command-t']
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = "Briefly highlight yanked text",
+  callback = function() vim.highlight.on_yank{higroup="IncSearch", timeout=400} end,
+})
 
-function! ShouldColor() abort
-  if index(g:WincentColorColumnBufferNameBlacklist, bufname(bufnr('%'))) != -1
-    return 0
-  endif
-  return index(g:WincentColorColumnFileTypeBlacklist, &filetype) == -1
-endfunction
-
-function! ShouldCursorline() abort
-  return index(g:WincentCursorlineBlacklist, &filetype) == -1
-endfunction
-
-function! BlurBuffer() abort
-  if ShouldColor()
-    ownsyntax off
-  endif
-endfunction
-
-function! FocusBuffer() abort
-  if ShouldColor()
-    if !empty(&ft)
-      ownsyntax on
-    endif
-  endif
-endfunction
-
-autocmd BufEnter,FocusGained,VimEnter,WinEnter * call FocusBuffer()
-autocmd FocusLost,WinLeave * call BlurBuffer()
-
-" save file on blur
-autocmd FocusLost,WinLeave * silent! :wa
-
-" auto highlight yanked content
-" https://jdhao.github.io/2020/05/22/highlight_yank_region_nvim/
-augroup highlight_yank
-  autocmd!
-  au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=400}
-augroup END
-
-" When entering insert mode, relative line numbers are turned off
-" Same when the buffer loses focus
-" see https://jeffkreeftmeijer.com/vim-number/
-augroup numbertoggle
-  autocmd!
-  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
-  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
-augroup END
-
-" disable folds inside Telesope picker
-autocmd! FileType TelescopeResults setlocal nofoldenable
-]])
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Avoid folds inside Telesope picker",
+  pattern = "TelescopeResults",
+  command = [[setlocal nofoldenable]],
+})
