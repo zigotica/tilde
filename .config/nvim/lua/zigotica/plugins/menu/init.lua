801e176974233bdffba90da6d93c89a721d40cbd
zigotica <zigotica@gmail.com>
2023-11-02 22:00:02 +0100

    improve readability of autocmd for save on blur

--- .config/nvim/lua/zigotica/plugins/menu/init.lua
+++ .config/nvim/lua/zigotica/plugins/menu/init.lua
@@ -83,5 +83,9 @@ local function open_nvim_tree(data)
   -- open the tree
   require("nvim-tree.api").tree.open()
 end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "open menu when entering neovim",
+  callback = open_nvim_tree
+})
 
