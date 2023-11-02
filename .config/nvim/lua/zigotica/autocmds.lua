4ce91e95e070022ab2063b766cebb2e5c605b47e
zigotica <zigotica@gmail.com>
2023-11-02 13:01:08 +0100

    recover autocmd to save on blur, in lua syntax

--- .config/nvim/lua/zigotica/autocmds.lua
+++ .config/nvim/lua/zigotica/autocmds.lua
@@ -8,3 +8,10 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "TelescopeResults",
   command = [[setlocal nofoldenable]],
 })
+
+vim.api.nvim_create_autocmd("FocusLost,WinLeave", {
+  desc = "Save files on blur",
+  pattern = "*",
+  command = "silent! :wa",
+})
+
