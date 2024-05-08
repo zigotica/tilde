c923bc69d76fd65585adf56c47102dfaeffb2f40
zigotica <zigotica@gmail.com>
2024-05-08 16:20:58 +0200

    fix autocmds syntax due to update

--- .config/nvim/lua/zigotica/autocmds.lua
+++ .config/nvim/lua/zigotica/autocmds.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
+vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Briefly highlight yanked text",
   callback = function() vim.highlight.on_yank{higroup="IncSearch", timeout=400} end,
 })
@@ -9,13 +9,13 @@ vim.api.nvim_create_autocmd("FileType", {
   command = [[setlocal nofoldenable]],
 })
 
-vim.api.nvim_create_autocmd("FocusLost,WinLeave", {
+vim.api.nvim_create_autocmd({"FocusLost","WinLeave"}, {
   desc = "Save files on blur",
   pattern = "*",
   command = "silent! :wa",
 })
 
-vim.api.nvim_create_autocmd("BufRead,BufNewFile", {
+vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
   desc = "Interpret inventory files as an Ansible inventory",
   pattern = "*/**/inventory",
   command = "set filetype=yaml.ansible",
