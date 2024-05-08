b271c642340037633fd53feaf97f371bb3a37125
zigotica <zigotica@gmail.com>
2024-05-08 16:54:05 +0200

    remove eslint from nullls diagnostics config

--- .config/nvim/lua/zigotica/plugins/nullls/init.lua
+++ .config/nvim/lua/zigotica/plugins/nullls/init.lua
@@ -4,7 +4,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
   },
 })
