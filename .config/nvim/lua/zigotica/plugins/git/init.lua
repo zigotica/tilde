514fa08234ab3d22dddb435a260daec5a041d554
zigotica <zigotica@gmail.com>
2024-08-20 18:12:01 +0200

    gitsigns options change of syntax

--- .config/nvim/lua/zigotica/plugins/git/init.lua
+++ .config/nvim/lua/zigotica/plugins/git/init.lua
@@ -4,10 +4,10 @@ vim.cmd[[ highlight GitSignsDelete guifg=#c98b65 ]]
 
 require('gitsigns').setup{
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '≃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = {text = '+'},
+    change       = {text = '~'},
+    delete       = {text = '-'},
+    topdelete    = {text = '‾'},
+    changedelete = {text = '≃'},
   },
 }
