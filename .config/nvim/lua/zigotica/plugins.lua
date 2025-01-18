18c5789bf6ad99ab42990ebd1b5d6878a32e5961
zigotica <zigotica@gmail.com>
2025-01-18 23:29:05 +0100

    nvim plugins format file

--- .config/nvim/lua/zigotica/plugins.lua
+++ .config/nvim/lua/zigotica/plugins.lua
@@ -13,21 +13,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- lazy requires leader key to be defined before plugins are required
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- list of plugins and dependencies
 require("lazy").setup({
   -- Basic extensions
-  'tpope/vim-surround',
-  'mg979/vim-visual-multi',
-  'numToStr/Comment.nvim',
+  "tpope/vim-surround",
+  "mg979/vim-visual-multi",
+  "numToStr/Comment.nvim",
 
   -- Telescope core
   {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      "nvim-lua/plenary.nvim",
     },
   },
 
@@ -35,128 +35,131 @@ require("lazy").setup({
   {
     -- useful for days when the z12 micropad w/ rotary encoder not at hand
     -- https://github.com/zigotica/mechanical-keyboards/tree/main/z12
-    'debugloop/telescope-undo.nvim',
+    "debugloop/telescope-undo.nvim",
     dependencies = {
-      'nvim-telescope/telescope.nvim',
-    }
+      "nvim-telescope/telescope.nvim",
+    },
   },
   {
-    'nvim-telescope/telescope-symbols.nvim',
+    "nvim-telescope/telescope-symbols.nvim",
     dependencies = {
-      'nvim-telescope/telescope.nvim',
-    }
+      "nvim-telescope/telescope.nvim",
+    },
   },
   {
-    'ghassan0/telescope-glyph.nvim',
+    "ghassan0/telescope-glyph.nvim",
     dependencies = {
-      'nvim-telescope/telescope.nvim',
-    }
+      "nvim-telescope/telescope.nvim",
+    },
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     dependencies = {
-      'nvim-telescope/telescope.nvim',
-    }
+      "nvim-telescope/telescope.nvim",
+    },
   },
 
   -- FZF direct replacement for telescope find_files (was too slow)
   {
-    'ibhagwan/fzf-lua',
+    "ibhagwan/fzf-lua",
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
-      { 'junegunn/fzf', build = './install --bin' }
-    }
+      "nvim-tree/nvim-web-devicons",
+      { "junegunn/fzf", build = "./install --bin" },
+    },
   },
 
   -- Languages
-  'neovim/nvim-lspconfig',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/nvim-cmp',
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  'hrsh7th/cmp-path',
-  'jxnblk/vim-mdx-js',
+  "neovim/nvim-lspconfig",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/nvim-cmp",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "hrsh7th/cmp-path",
+  "jxnblk/vim-mdx-js",
   {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
   },
   {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
   },
   {
-    'nvimtools/none-ls.nvim',
-    dependencies = 'nvim-lua/plenary.nvim',
+    "nvimtools/none-ls.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
   },
-  'windwp/nvim-autopairs',
+  "windwp/nvim-autopairs",
   {
-    'windwp/nvim-ts-autotag',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
   },
-  'mattn/emmet-vim',
-  'editorconfig/editorconfig-vim',
+  "mattn/emmet-vim",
+  "editorconfig/editorconfig-vim",
   {
-    'kevinhwang91/nvim-bqf',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
+    "kevinhwang91/nvim-bqf",
+    dependencies = "nvim-treesitter/nvim-treesitter",
   },
-  'pearofducks/ansible-vim',
+  "pearofducks/ansible-vim",
 
   -- AI completion
   {
-    'tzachar/cmp-tabnine',
-    build = './install.sh',
-    dependencies = 'hrsh7th/nvim-cmp',
+    "tzachar/cmp-tabnine",
+    build = "./install.sh",
+    dependencies = "hrsh7th/nvim-cmp",
   },
   {
-    'Exafunction/codeium.nvim',
+    "Exafunction/codeium.nvim",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'hrsh7th/nvim-cmp',
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
     },
   },
-  { 'David-Kunz/gen.nvim' },
+
+  -- AI menu with code and english actions
+  { "David-Kunz/gen.nvim" },
 
   -- Testing
   {
-    'nvim-neotest/neotest',
+    "nvim-neotest/neotest",
     dependencies = {
-      'nvim-neotest/nvim-nio',
-      'haydenmeade/neotest-jest',
-      'antoinemadec/FixCursorHold.nvim',
+      "nvim-neotest/nvim-nio",
+      "haydenmeade/neotest-jest",
+      "antoinemadec/FixCursorHold.nvim",
     },
-    ft = { 'js', 'ts', 'jsx', 'tsx' },
+    ft = { "js", "ts", "jsx", "tsx" },
   },
 
   -- Git
-  'lewis6991/gitsigns.nvim',
+  "lewis6991/gitsigns.nvim",
 
   -- Colors
-  'edeneast/nightfox.nvim',
-  'norcalli/nvim-colorizer.lua',
+  "edeneast/nightfox.nvim",
+  "norcalli/nvim-colorizer.lua",
 
   -- Markdown viewer; other options:
   --   "OXY2DEV/markview.nvim",
   --   "toppair/peek.nvim",
   {
-    'MeanderingProgrammer/render-markdown.nvim',
+    "MeanderingProgrammer/render-markdown.nvim",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons'
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
     },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
   },
 
-  -- Menu 
+  -- Menu
   {
-    'kyazdani42/nvim-tree.lua',
-    dependencies = 'kyazdani42/nvim-web-devicons',
+    "kyazdani42/nvim-tree.lua",
+    dependencies = "kyazdani42/nvim-web-devicons",
   },
 
   -- Status
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = 'kyazdani42/nvim-web-devicons',
+    "nvim-lualine/lualine.nvim",
+    dependencies = "kyazdani42/nvim-web-devicons",
   },
 })
